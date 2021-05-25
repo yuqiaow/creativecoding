8605b921aa27b9e1ece3ed820ebd7638f4591901
yuqiaow <54876343+yuqiaow@users.noreply.github.com>
2021-05-25 16:13:59 -0400

    Add files via upload

---
+++ steering behavior/sketch.js
@@ -0,0 +1,45 @@
+
+// Daniel Shiffman
+// http://codingtra.in
+// Steering Text Paths
+// Video: https://www.youtube.com/watch?v=4hA7G3gup-4
+
+var font;
+var vehicles = [];
+
+function preload() {
+  font = loadFont('Lato-Bold.ttf');
+}
+
+function setup() {
+  createCanvas(1600, 300);
+  background(51);
+  // textFont(font);
+  // textSize(192);
+  // fill(255);
+  // noStroke();
+  // text('train', 100, 200);
+
+  var points = font.textToPoints('Down the rabbit hole', 200, 200, 66, {
+    sampleFactor: 0.35
+  });
+
+  for (var i = 0; i < points.length; i++) {
+    var pt = points[i];
+    var vehicle = new Vehicle(pt.x, pt.y);
+    vehicles.push(vehicle);
+    // stroke(255);
+    // strokeWeight(8);
+    // point(pt.x, pt.y);
+  }
+}
+
+function draw() {
+  background(255);
+  for (var i = 0; i < vehicles.length; i++) {
+    var v = vehicles[i];
+    v.behaviors();
+    v.update();
+    v.show();
+  }
+}
